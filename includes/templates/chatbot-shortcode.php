3b2f19ff2911f44e5f488a18cf73bab3db213b43
Tejas Bramhecha <sci.tj.research@gmail.com>
2017-07-24 14:51:44 +0530

    Update chatbot-shortcode.php

--- includes/templates/chatbot-shortcode.php
+++ includes/templates/chatbot-shortcode.php
@@ -1,29 +1,30 @@
 <button id="demo" onclick="myFunction()">Click me</button>
 
-<div class="myc-container" id="test" hidden="hidden">
-	<div id="myc-conversation-area"></div>
-	<div id="myc-input-area">
-		<input id="myc-text" type="text" placeholder="<?php _e( 'Ask something...', 'my-chatbot' ); ?>"></input>
-	</div>
+<div id="myc-container" hidden="hidden">
+<div id="myc-conversation-area" ></div>
+<div id="myc-input-area" >
+		<input id="myc-text" type="text" placeholder="Ask something..."></input>
 </div>
-
+</div>
+	
 <script>
 var flag = "true";
-function myFunction() 
-{
-    	if(flag == "true")
+console.log(flag);
+function myFunction() {
+    if(flag == "true")
 	{
 		document.getElementById('myc-container').hidden="";
 		flag = "false"
+		console.log("not hidding ");
 	}
 	else
 	{
 		document.getElementById('myc-container').hidden="hidden";
 		flag = "true";
+		console.log("hidding " + flag);
 	}
 }
 </script>
-
 <?php if ( $debug ) { ?>
 	<div class="myc-debug">
 		<textarea id="myc-debug-data" cols="80" rows="20" disabled></textarea>
